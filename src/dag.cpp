1e84b63f533b44bb891932d9f2380a4c6c82edb5
Martin Sandrock <msandrock@gmx.de>
2019-10-13 00:19:40 +0200

    Basic dag logic functions

--- src/dag.cpp
+++ src/dag.cpp
@@ -4,8 +4,42 @@
 #include "dag.hpp"
 
 namespace dag {
+
+    Dependency* find_ancestor(const Dependency& current, const std::vector<Dependency>& dependencies) {
+        Dependency* ancestor = nullptr; 
+        // Loop over all dependencies and find the ancestor node
+        for(auto dependency: dependencies) {
+            if (dependency.child_hash == current.ancestor_hash) {
+                ancestor = &dependency;
+                break;
+            }
+        }
+
+        return ancestor;
+    }
+    
     std::vector<DagNode> build_dag(const std::vector<Dependency>& dependencies) {
         std::vector<DagNode> startNodes;
+
+        // 1. Every node that is not a child node is automatically a start node
+        for (auto dependency: dependencies) {
+            auto ancestor = find_ancestor(dependency, dependencies);
+            
+            if (ancestor == nullptr) {
+                std::cout << "Node " << dependency.ancestor << " has no ancestor" << std::endl;
+                /*std::unique_ptr<DagNode> startNode(new DagNode());
+                startNode->dependency = &dependency;
+                dependency.node = &startNode;
+                startNodes->push_back(startNode);*/
+            } else {
+                std::cout << "Node " << dependency.ancestor << " has an ancestor" << std::endl;
+            }
+        }
+
+        // Compare every node with the others - if it is not a child node, then it's a start node
+            // once assigned, flag it as consumed
+
+        // 2. Every node that references an ancestor node is invalid
 
         return startNodes;
     }

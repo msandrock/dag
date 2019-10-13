932f9e1c089682260ea6d8bad14ba230206a423c
Martin Sandrock <msandrock@gmx.de>
2019-10-13 17:22:36 +0200

    More dag building logic

--- src/dag.cpp
+++ src/dag.cpp
@@ -21,6 +21,33 @@ namespace dag {
     }
 
     /**
+     * Append direct child nodes to the current node
+     */ 
+    void append_dependencies(DagNode& currentNode, std::vector<Dependency>& dependencies) {
+        // Find all nodes that have the current node as a parent
+        for (auto dependency: dependencies) {
+            // No self-referencing
+            if (dependency.name_hash == currentNode.dependency.name_hash) {
+                continue;
+            }
+
+            Dependency* ancestor = nullptr;
+            find_upstream_dependency(dependency, dependencies, &ancestor);
+
+            // Dependency has no ancestor
+            if (ancestor == nullptr) {
+                continue;
+            }
+
+            // Is the start node the ancestor?
+            if (ancestor->name_hash == currentNode.dependency.name_hash) {
+                // Add the current node as a child
+                std::cout << dependency.name << " is a direct child of " << currentNode.dependency.name << std::endl;
+            }
+        }
+    }
+    
+    /**
      * Construct dag from the given dependencies
      */ 
     std::vector<DagNode> build_dag(std::vector<Dependency>& dependencies) {
@@ -39,34 +66,10 @@ namespace dag {
 
         // Construct dags for all start nodes
         for (auto startNode: startNodes) {
-            std::cout << "Building dag for " << startNode.dependency.name << std::endl;
-            // Find all nodes that have the start node as a parent
-            for (auto dependency: dependencies) {
-                // Skip root nodes
-                if (dependency.name_hash == startNode.dependency.name_hash) {
-                    continue;
-                }
-
-                std::cout << "Finding ancestor for " << dependency.name << std::endl;
-                Dependency* ancestor = nullptr;
-                find_upstream_dependency(dependency, dependencies, &ancestor);
-
-                // Current node has an ancestor
-                if (ancestor != nullptr) {
-                    std::cout << "Found an ancestor for " << dependency.name << std::endl;
-                    std::cout << (*ancestor).name_hash << " == " << startNode.dependency.name_hash << "?" << std::endl;
-
-                    // Is the start node the ancestor?
-                    /*if (ancestor->name == startNode.dependency.name) {
-                        // Add the current node as a child
-                        std::cout << dependency.name << " is a direct child of " << startNode.dependency.name << std::endl;
-                    }*/
-                } else {
-                    std::cout << "Could not find an ancestor for " << dependency.name << std::endl;
-                }
-            }
+            // Pass in the current dag - add all nodes to the end nodes
+            // dag, current node, dependencies
+            append_dependencies(startNode, dependencies);
         }
-
 
         // Compare every node with the others - if it is not a child node, then it's a start node
             // once assigned, flag it as consumed

9d688b0cb243a24deac8319ba4caa454e8b0a16a
Martin Sandrock <msandrock@gmx.de>
2019-10-29 19:43:46 +0100

    Improve rendering for large dags

--- svg.cpp
+++ svg.cpp
@@ -1,17 +1,28 @@
 #include <fstream>
+#include <set>
 #include <string>
 #include <vector>
 #include "svg.hpp"
 #include "dag.hpp"
 
+const int OFFSET = 25;  // Offset to canvas corner
+const int WIDTH = 240;
+const int HEIGHT = 40;
+const int XOFFSET = WIDTH + 100;
+const int YOFFSET = HEIGHT + 50;
+const int LABEL_MAX_LENGTH = 23;
+
 /*
  * Emit markup for a single dependency node
  */
-void _write_node(std::fstream& stream, const dag::node_ptr& node) {
-    const int OFFSET = 25;  // Offset to canvas corner
-    const int WIDTH = 240, HEIGHT = 40;
-    const int XOFFSET = WIDTH + 100;
-    const int YOFFSET = HEIGHT + 50;
+bool _write_node(std::fstream& stream,
+    std::set<std::string>& renderedNodes,
+    const dag::node_ptr& node) {
+
+    if (renderedNodes.find(node->name) != renderedNodes.end()) {
+        // Node already present in set
+        return false;
+    }
 
     // Source: https://stackoverflow.com/questions/5546346/how-to-place-and-center-text-in-an-svg-rectangle/44857272#44857272
     /*
@@ -21,34 +32,47 @@ void _write_node(std::fstream& stream, const dag::node_ptr& node) {
     stream << "</svg>" << std::endl;
     */
 
+    std::string nodeLabel = node->name;
+    if (nodeLabel.length() > LABEL_MAX_LENGTH) {
+        nodeLabel = nodeLabel.substr(0, LABEL_MAX_LENGTH) + "...";
+    }
+
     stream << "<rect x=\"" << node->x * XOFFSET + OFFSET << "\" y=\"" << node->y * YOFFSET + OFFSET << "\" width=\"" << WIDTH << "\" height=\"" << HEIGHT << "\" />" << std::endl;
-    //stream << "<ellipse cx=\"" << node->x * XOFFSET + OFFSET + WIDTH / 2 << "\" cy=\"" << node->y * YOFFSET + OFFSET + HEIGHT / 2 << "\" rx=\"" << WIDTH / 2 << "\" ry=\"" << HEIGHT / 2 << "\" />" << std::endl;
-    stream << "<text x=\"" << (node->x * XOFFSET + WIDTH / 2 - 90 + OFFSET) << "\" y=\"" << (node->y * YOFFSET + 5 + HEIGHT / 2 + OFFSET) << "\">" << node->name << "</text>" << std::endl;
+    stream << "<text x=\"" << (node->x * XOFFSET + WIDTH / 2 - 90 + OFFSET) << "\" y=\"" << (node->y * YOFFSET + 5 + HEIGHT / 2 + OFFSET) << "\">" << nodeLabel << "</text>" << std::endl;
+
+    renderedNodes.insert(node->name);
+    return true;
 }
 
 /**
  * Emit markup to draw a connecting line between two nodes
  */
-void _write_edge(std::fstream& stream, const dag::node_ptr& nodeStart, const dag::node_ptr& nodeEnd) {
-    const int OFFSET = 25;  // Offset to canvas corner
-    const int WIDTH = 240, HEIGHT = 40;
-    const int XOFFSET = WIDTH + 100;
-    const int YOFFSET = HEIGHT + 50;
+void _write_edge(
+    std::fstream& stream,
+    const dag::node_ptr& nodeStart,
+    const dag::node_ptr& nodeEnd) {
+    
     stream << "<line x1=\"" << (nodeStart->x * XOFFSET + WIDTH + OFFSET) << "\" y1=\"" << (nodeStart->y * YOFFSET + 5 + HEIGHT / 2 + OFFSET) << "\" x2=\""<< (nodeEnd->x * XOFFSET + OFFSET) << "\" y2=\"" << (nodeEnd->y * YOFFSET + 5 + HEIGHT / 2 + OFFSET) << "\" marker-end=\"url(#arrow)\" />" << std::endl;
 }
 
 /**
  * Recursively emit markup for a list of nodes
  */
-void _write_node_array(std::fstream& stream, const dag::node_vec& nodes, const dag::node_ptr& parentNode = nullptr) {
+void _write_node_array(
+    std::fstream& stream,
+    std::set<std::string>& renderedNodes,
+    const dag::node_vec& nodes,
+    const dag::node_ptr& parentNode = nullptr) {
+    
     for (auto node: nodes) {
         // Draw edge from parent node to connected node
         if (parentNode != nullptr) {
             _write_edge(stream, parentNode, node);
         }
 
-        _write_node(stream, node);
-        _write_node_array(stream, node->children, node);
+        if (_write_node(stream, renderedNodes, node)) {
+            _write_node_array(stream, renderedNodes, node->children, node);
+        }
     }
 }
 
@@ -121,7 +145,8 @@ void write_svg(const dag::node_vec& startNodes, const std::string& filename) {
     stream << "line { stroke: #f00; }" << std::endl;
     stream << "</style>" << std::endl;
 
-    _write_node_array(stream, startNodes);
+    std::set<std::string> renderedNodes;
+    _write_node_array(stream, renderedNodes, startNodes);
 
     stream << "</svg>";
 
